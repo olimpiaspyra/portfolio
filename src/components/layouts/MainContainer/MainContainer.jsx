a5d2d4a760104b011c7f56955dd454efe7b1d182
Olimpia Spyra <olimpia.spyra@gmail.com>
2023-03-17 22:10:59 +0100

    Add hero component

--- src/components/layouts/MainContainer/MainContainer.jsx
+++ src/components/layouts/MainContainer/MainContainer.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledMainContainer = styled.main`
-width: 100%;
-max-width: 1600px;
-margin: 0 auto;
-padding: 200px 150px;
+import HeroSection from '../../pages/HeroSection/HeroSection';
 
-  p {
-  text-align: center;
-}
+const StyledMainContainer = styled.main`
+  width: 100%;
+  max-width: 1600px;
+  margin: 0 auto;
+  padding: 300px 150px;
 `;
 
 const MainContainer = () => {
   return (
     <StyledMainContainer>
-      <p>text</p>
+      <HeroSection>
+      </HeroSection>
     </StyledMainContainer>
   );
 };
---
+++ src/components/pages/HeroSection/HeroSection.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.section`
+  max-width: 1600px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 50px;
+  min-height: 100vh;
+
+  @media screen and (max-width: 1200px) {
+    padding: 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0;
+    text-align: center;
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+
+  h2 {
+    margin: 0 0 20px 4px;
+    color: var(--lightest-slate);
+    font-size: 6.5rem;
+    font-weight: 600;
+    line-height: 1.1;
+
+    @media screen and (max-width: 1200px) {
+      font-size: 5rem;
+    }
+
+
+    @media screen and (max-width: 768px) {
+      font-size: 4rem;
+    }
+
+    @media screen and (max-width: 600px) {
+      font-size: 3rem;
+    }
+  }
+
+  h3 {
+    margin: 0 0 5px 4px;
+    color: var(--slate);
+    line-height: 0.9;
+    font-size: 6rem;
+
+    @media screen and (max-width: 1200px) {
+      font-size: 4rem;
+    }
+
+    @media screen and (max-width: 768px) {
+      font-size: 3rem;
+    }
+
+    @media screen and (max-width: 600px) {
+      font-size: 2rem;
+    }
+  }
+
+  p {
+    margin: 20px 0 0 4px;
+    max-width: 540px;
+    font-size: 2rem;
+  }
+}
+`;
+
+const HeroSection = () => {
+  return (
+    <StyledContainer>
+      <h1>Hi, my name is</h1>
+      <h2>Olimpia Spyra.</h2>
+      <h3>I build a modern website.</h3>
+      <p>I am junior front end developer and I am constantly improving my skills.</p>
+    </StyledContainer>
+  );
+};
+
+export default HeroSection;
